0b7366b122b46340347ef8451cd440b9ad97ace2
Jeff Czorapinski <jczorapinski@gmail.com>
2016-11-20 21:54:32 -0500

    Added some more Hazelcast work.

--- src/main/java/microservices/Application.java
+++ src/main/java/microservices/Application.java
@@ -1,20 +1,58 @@
 package microservices;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.hawt.config.ConfigFacade;
+import io.hawt.springboot.EnableHawtio;
+import io.hawt.system.ConfigManager;
+import io.hawt.web.AuthenticationFilter;
 
 /**
  * @author jeff
  *
  */
 @SpringBootApplication
+@EnableHawtio
 public class Application {
+	@Autowired
+	private ServletContext servletContext;
 
 	/**
 	 * @param args
 	 * @throws Exception
 	 */
 	public static void main(String... args) throws Exception {
+		System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
 		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		final ConfigManager configManager = new ConfigManager();
+		configManager.init();
+		servletContext.setAttribute("ConfigManager", configManager);
+	}
+
+	/**
+	 * Set things up to be in offline mode
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public ConfigFacade configFacade() throws Exception {
+		ConfigFacade config = new ConfigFacade() {
+			public boolean isOffline() {
+				return true;
+			}
+		};
+		config.init();
+		return config;
 	}
 }
--- src/main/java/microservices/routes/MessagingBuilder.java
+++ src/main/java/microservices/routes/MessagingBuilder.java
@@ -1,10 +1,14 @@
 package microservices.routes;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.hazelcast.HazelcastConstants;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import com.hazelcast.collection.common.DataAwareItemEvent;
 
 /**
  * @author jeff
@@ -38,10 +42,17 @@ public class MessagingBuilder extends RouteBuilder {
 			.choice()
 				.when(header(HazelcastConstants.LISTENER_ACTION).isEqualTo(HazelcastConstants.ADDED))
 					.log(LoggingLevel.INFO, "Got new message -> ${body}")
-					.to("mock:added")
+					.process( new Processor() {
+						
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							Object messageBody = exchange.getIn().getBody();
+							DataAwareItemEvent event = (DataAwareItemEvent)messageBody;
+							System.out.println(event.getItem());
+						}
+					})
 				.when(header(HazelcastConstants.LISTENER_ACTION).isEqualTo(HazelcastConstants.REMOVED))
 					.log(LoggingLevel.INFO, "Bye bye message -> ${body}")
-					.to("mock:removed")
 				.otherwise()
 					.log(LoggingLevel.WARN, "WTFJH :-/");
 			
